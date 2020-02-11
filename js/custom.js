90af37cbb17a393831032b9a92f4811b14d975f2
marisqq <marisvitolsm@gmail.com>
2020-02-11 17:45:44 +0200

    generates 100 divs different ids

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,46 @@
-console.log("Staring div generator")
+// console.log("Staring div generator")
+// for (let i=1; i <= 100; i++)
+// {let newDiv = document.createElement("div")
+// let newPar = document.createElement("p");
+// newDiv.appendChild(newPar);
+//         main.appendChild(newDiv).setAttribute("id", "div_" + i); 
+
+
+
+//     if (i % 15 == 0)
+//         return("FizzBuzz");
+       
+//     else if (i % 3 == 0)
+//         console.log("Fizz");
+//     else if (i % 5 == 0)
+//         console.log("Buzz");
+//     else
+//         console.log(i);
+// }
+// Generates into console
+
+for (let i = 1; i <=100; i++){
+
+    
+var counter = 1;  //doesnt work          //generates 100 divs with different id
+$("#generate-div").click(function(){
+    let $newDiv = $(("<div></div>"));
+    $("div").each(function(i) {
+        $(this).find("span").text(++i);
+    });
+    $newDiv.attr("id", "newDiv" + counter++);
+    $(".main").append($newDiv);
+}); 
+
+}  
+// for (let i = 1; i <=100; i++){
+// if (i % 15 == 0)
+// $(".main").append("<span class='fizzbuzz'>FizzBuzz</span>");
+       
+//     else if (i % 3 == 0)
+//     $(".main").append("<span class='fizz'>Fizz</span>");
+//     else if (i % 5 == 0)
+//     $(".main").append("<span class='buzz'>Buzz</span>");
+//     else
+//     $(".main").append("<span>" + i + "</span>");
+// }
